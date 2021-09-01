8bd7138214bc1bd98d8c01595ccdbb6b3f5098a9
John Nkereuwem <john.nkereuwem@outlook.com>
2021-09-01 16:09:27 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let toggleBtn = document.querySelector('.toggler');
+let nav = document.querySelector('#nav');
+let links = document.querySelectorAll('.link');
+
+
+    toggleBtn.addEventListener('click', ()=>{
+        toggleBtn.classList.toggle('active');
+        nav.classList.toggle('active');
+    })
+
+
+
+
+// for(let i = 0; i < links.length; i++){
+//    let item = links[i];
+//    item.addEventListener('click', ()=>{
+//     toggleBtn.classList.toggle('active');
+//     nav.classList.toggle('active');})
+// }
+
+
+
+   
+
+
+
+
