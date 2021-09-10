1feb3ce1a56ba6133d910ca2b92f9ee8b032f171
John Nkereuwem <john.nkereuwem@outlook.com>
2021-09-10 13:29:49 +0100

    Second commit

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
-let toggleBtn = document.querySelector('.toggler');
-let nav = document.querySelector('#nav');
-let links = document.querySelectorAll('.link');
+let btn = document.querySelector('.toggle');
+let span = document.querySelectorAll('span')
+let nav = document.querySelector('.nav');
 
 
-    toggleBtn.addEventListener('click', ()=>{
-        toggleBtn.classList.toggle('active');
-        nav.classList.toggle('active');
+btn.addEventListener('click', function(){
+    nav.classList.toggle('active');
+    span.forEach(function(tog){
+        return tog.classList.toggle('active');
     })
+})
+
+function myMap(){
+    let mapProp = {
+        center:new google.maps.LatLng(9.051758756652701, 7.4436299758208575),
+        zoom:5
+    };
+    const map = new google.maps.Map(document.getElementById('map'), mapProp)
+}
+
+// var googlemap = new google.maps.Map(document.getElementById("map"), mapOptions);
+//             var marker = new google.maps.Marker({
+//                 position: siliconValley,
+//                 map: googlemap
+//             });
 
 
-
-
-// for(let i = 0; i < links.length; i++){
-//    let item = links[i];
-//    item.addEventListener('click', ()=>{
-//     toggleBtn.classList.toggle('active');
-//     nav.classList.toggle('active');})
-// }
 
 
 
